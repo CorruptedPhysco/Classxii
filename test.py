6178cff69dc0181f09e2fb86084c1f1f82d029b0
CorruptedPhysco <106505409+CorruptedPhysco@users.noreply.github.com>
2024-08-17 10:51:26 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 def add_emp():
   with open('emp.csv', 'a', newline='') as f:
     W=csv.writer(f)
-    W.writerow(['employee number','
+    W.writerow(['employee number','employee name','salary'])
+    name = input("Enter Employee Name:")
+    empno = int(input("Enter Employee Number:"))
